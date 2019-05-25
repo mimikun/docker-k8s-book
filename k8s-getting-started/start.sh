cf34bea87cd8ba7f2c0747d3ca819e909ee3a247
mimikun <karaage_nennen@icloud.com>
2019-05-25 23:01:19 +0900

    5.5まで終わった

--- k8s-getting-started/start.sh
+++ k8s-getting-started/start.sh
@@ -19,4 +19,27 @@ kubectl proxy
 #http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/overview?namespace=default
 
 ## 5.3 k8sの概念
-ここまで
+#使いそうな概念から
+#Node: k8sクラスタ内で実行するコンテナを配置するためのサーバ
+#Namespace: k8sクラスタ内で作る仮想的なクラスタ
+#Pod: コンテナ集合体の単位で、コンテナを実行する方法を定義する
+#Service: Podの集合にアクセスするための経路を定義する
+#Ingress: Serviceをk8sクラスタの外に公開する
+
+## 5.4 k8sクラスタとNode
+#Node...クラスタが持つリソースで最も大きな概念。
+#k8sクラスタは、masterとNode群によって構成される
+
+#このコマンドで、クラスタに参加しているNodeの一覧を取得できる
+kubectl get nodes
+
+## 5.5 Namespace
+#このコマンドでクラスタが持つNamespaceの一覧を取得できる
+kubectl get namespace
+
+## 5.6 Pod
+#k8sをDockerと組み合わせる場合、Podが持つのはDockerコンテナ単体あるいはDockerコンテナの集合
+#同一Pod内のコンテナはすべて同一のNodeに配置される
+
+### 5.6.1 Podを作成してデプロイする
+simple-pod.ymlをつかう ここまで
